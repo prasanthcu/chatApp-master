7696641bf1335a2eb7fe9d64e066dee1a15c82d4
prasantu <mailcuprasanth@gmail.com>
2017-08-22 20:41:53 +0530

    PairScreen login addition

---
+++ app/src/main/java/com/mychat/prasanth/InternalCode.java
@@ -0,0 +1,18 @@
+package com.mychat.prasanth;
+
+/**
+ * Created by prasantu on 22-Aug-17.
+ */
+
+public class InternalCode {
+
+    String mInternalCode;
+
+    public InternalCode(String mInternalCode){
+        this.mInternalCode = mInternalCode;
+    }
+
+    public InternalCode(){
+
+    }
+}
